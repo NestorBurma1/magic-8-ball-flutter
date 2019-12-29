316249a5a407db565546b28f3c9f35136fa5b95d
Oleksii <alexeyhoroshunvk@gmail.com>
2019-12-29 16:54:47 +0200

    Migrate to AndroidX

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() =>
+    runApp(
       MaterialApp(
-        home: null,
+        home: Ball(),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Text('Ask Me Anything'),
+      ),
+      body: Center(
+        child: Container(
+          child: FlatButton(
+            child: Image.asset('images/ball1.png'),
+              onPressed: setState(() {
+                print('I got clicked');
+              }); ),
+        ),
+      ),
+    );
+  }
+}
